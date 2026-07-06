a5b4c00bf921c1019aa4c259d2945ee239594a25
podzemniytip <tregub87@bk.ru>
2026-07-06 18:00:25 +0300

    deploy: frontend

---
+++ scripts/check-balance.js
@@ -0,0 +1,10 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  const balance = await hre.ethers.provider.getBalance(deployer.address);
+  console.log("Wallet:", deployer.address);
+  console.log("Balance:", hre.ethers.formatEther(balance), "ETH");
+}
+
+main().catch(console.error);
